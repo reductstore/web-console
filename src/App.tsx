c216e7224b1c6046fe8dc5c8a942f79a65a6c979
Aleksey Timin <atimin@gmail.com>
2022-04-20 02:39:54 +0200

    get storage url from location or env

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,8 @@ import {Client} from "reduct-js";
 import {Container} from "semantic-ui-react";
 
 function App() {
-    const client = new Client("http://localhost:8383");
+    const client = new Client(process.env.REACT_APP_STORAGE_URL || window.location.toString());
+
     return (
         <div className="App">
             <Container>
