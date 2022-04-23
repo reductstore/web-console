cdb4b65dae4acb651bab9bcefe6d62b9b3dd1804
Aleksey Timin <atimin@gmail.com>
2022-04-24 00:47:09 +0200

    public_url = ui/

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,15 @@ import {Client} from "reduct-js";
 import {Container} from "semantic-ui-react";
 
 function App() {
-    const client = new Client(process.env.REACT_APP_STORAGE_URL || window.location.toString());
+    let url = process.env.REACT_APP_STORAGE_URL;
+    if (url === undefined) {
+        let path = window.location.pathname;
+        path = path.replace(process.env.PUBLIC_URL, "");
+        url = `${window.location.protocol}//${window.location.host}${path}`;
+    }
+
+    console.log(url);
+    const client = new Client(url);
 
     return (
         <div className="App">
