0e947ecf38dd48e6d55382ae3dd6e9aa0ab05bda
Aleksey Timin <atimin@gmail.com>
2022-07-16 19:19:48 +0200

    fix porblem with stream and crypto

---
+++ config-overrides.js
@@ -0,0 +1,19 @@
+module.exports = function override(config, env) {
+  console.log("override");
+  const loaders = config.resolve;
+  loaders.fallback = {
+    "fs": false,
+    "tls": false,
+    "net": false,
+    "http": false,
+    "https": false,
+    "zlib": false,
+    "path": false,
+    "stream": require.resolve("stream-browserify"),
+    "util": false,
+    "crypto": require.resolve("crypto-browserify"),
+    "buffer": require.resolve("buffer/"),
+  };
+
+  return config;
+};
