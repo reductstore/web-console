b4162230fbdcbf3451b06b34f1cbc8bf0c4f8486
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-03-22 14:05:57 +0100

    show license expiryDate in local format

--- src/Components/LicenseDetails/LicenseDetails.tsx
+++ src/Components/LicenseDetails/LicenseDetails.tsx
@@ -6,16 +6,19 @@ interface LicenseDetailsProps {
   license: LicenseInfo;
 }
 
-const LicenseDetails: React.FC<LicenseDetailsProps> = ({license}) => (
-  <Descriptions size="small" column={3}>
-    <Descriptions.Item label="Plan">{license.plan}</Descriptions.Item>
-    <Descriptions.Item label="Licensee">{license.licensee}</Descriptions.Item>
-    <Descriptions.Item label="Invoice">{license.invoice}</Descriptions.Item>
-    <Descriptions.Item label="Expiry Date">{license.expiryDate}</Descriptions.Item>
-    <Descriptions.Item label="Device Number">{license.deviceNumber}</Descriptions.Item>
-    <Descriptions.Item label="Disk Quota">{license.diskQuota}</Descriptions.Item>
-    <Descriptions.Item label="Fingerprint">{license.fingerprint}</Descriptions.Item>
-  </Descriptions>
-);
+const LicenseDetails: React.FC<LicenseDetailsProps> = ({license}) => {
+  const expiryDate = new Date(license.expiryDate).toLocaleDateString();
+  return (
+    <Descriptions size="small" column={3}>
+      <Descriptions.Item label="Plan">{license.plan}</Descriptions.Item>
+      <Descriptions.Item label="Licensee">{license.licensee}</Descriptions.Item>
+      <Descriptions.Item label="Invoice">{license.invoice}</Descriptions.Item>
+      <Descriptions.Item label="Expiry Date">{expiryDate}</Descriptions.Item>
+      <Descriptions.Item label="Device Number">{license.deviceNumber}</Descriptions.Item>
+      <Descriptions.Item label="Disk Quota">{license.diskQuota}</Descriptions.Item>
+      <Descriptions.Item label="Fingerprint">{license.fingerprint}</Descriptions.Item>
+    </Descriptions>
+  );
+};
 
 export default LicenseDetails;
