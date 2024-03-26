9c12c371bab896a2a5ec460181c7f4b0568769c7
AnthonyCvn <anthony.cavin.ac@gmail.com>
2024-03-26 12:18:23 +0100

    update license details

--- src/Components/LicenseAlert/LicenseAlert.tsx
+++ src/Components/LicenseAlert/LicenseAlert.tsx
@@ -2,24 +2,24 @@ import React from "react";
 import {Alert} from "antd";
 
 interface LicenseAlertProps {
-  alertExplanation?: string;
+  alertMessage?: React.ReactNode;
 }
 
-const LicenseAlert: React.FC<LicenseAlertProps> = ({alertExplanation}) => (
+const LicenseAlert: React.FC<LicenseAlertProps> = ({alertMessage}) => (
   <Alert
     type="warning"
     message={
-      <span>
-        {alertExplanation ? alertExplanation : ""}
-        Please review the <strong>
-          <a href="https://github.com/reductstore/reductstore/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">
-            Business Source License (BUSL)
-          </a></strong> for ReductStore and consult our <strong>
-          <a href="https://www.reduct.store/pricing" target="_blank" rel="noopener noreferrer">
-            pricing page
-          </a></strong> for more information on licensing options.
-      </span>
-    }
+      alertMessage || (
+        <span>
+          Please review the <strong>
+            <a href="https://github.com/reductstore/reductstore/blob/main/LICENSE" target="_blank" rel="noopener noreferrer">
+              Business Source License (BUSL)
+            </a></strong> for ReductStore and consult our <strong>
+            <a href="https://www.reduct.store/pricing" target="_blank" rel="noopener noreferrer">
+              pricing page
+            </a></strong> for more information on licensing options.
+        </span>
+      )}
   />
 );
 
--- src/Components/LicenseDetails/LicenseDetails.tsx
+++ src/Components/LicenseDetails/LicenseDetails.tsx
@@ -5,6 +5,8 @@ import "./LicenseDetails.css";
 import LicenseAlert from "../LicenseAlert/LicenseAlert";
 // @ts-ignore
 import prettierBytes from "prettier-bytes";
+import {bigintToNumber} from "../../Helpers/NumberUtils";
+import {checkLicenseStatus} from "../../Helpers/licenseUtils";
 
 interface LicenseDetailsProps {
   license: LicenseInfo;
@@ -12,6 +14,9 @@ interface LicenseDetailsProps {
 }
 
 const LicenseDetails: React.FC<LicenseDetailsProps> = ({license, usage}) => {
+  const {isValid, hasExpired, isOverQuota, usageInTB} = checkLicenseStatus(license, usage);
+  const isUnlimited = license.diskQuota === 0;
+  const usagePercentage = isUnlimited ? "N/A" : `${((usageInTB / Number(license.diskQuota)) * 100).toFixed(2)}%`;
   const expiryDate = new Date(license.expiryDate).toLocaleDateString(undefined,
     {
       year: "numeric",
@@ -19,22 +24,29 @@ const LicenseDetails: React.FC<LicenseDetailsProps> = ({license, usage}) => {
       day: "numeric",
     }
   );
-
-  const hasExpired = Date.now() > license.expiryDate;
-  const isOverQuota = usage > BigInt(license.diskQuota);
-  const alertExplanation = hasExpired ? "Your license has expired. " : isOverQuota ? "You have exceeded your disk quota. " : "";
-
+  const alertMessage = (
+    <span>
+      Your license {hasExpired ? "has expired" : "disk quota has been exceeded"}.
+      Please <strong><a href="https://www.reduct.store/contact" target="_blank" rel="noopener noreferrer">contact us</a>
+      </strong> to {hasExpired ? "renew your license" : "increase your disk quota"} at your earliest convenience.
+    </span>
+  );
   return (
     <>
-      {alertExplanation && <div className="licenseAlert"><LicenseAlert alertExplanation={alertExplanation} /></div>}
-      <Descriptions size="small" column={3} className="licenseDescriptions">
-        <Descriptions.Item label="Plan">{license.plan}</Descriptions.Item>
+      {!isValid && <div className="licenseAlert"><LicenseAlert alertMessage={alertMessage} /></div>}
+      <Descriptions size="default" column={1} className="licenseDescriptions">
         <Descriptions.Item label="Licensee">{license.licensee}</Descriptions.Item>
+        <Descriptions.Item label="Plan">{license.plan}</Descriptions.Item>
         <Descriptions.Item label="Invoice">{license.invoice}</Descriptions.Item>
-        <Descriptions.Item label="Expiry Date">{expiryDate}</Descriptions.Item>
         <Descriptions.Item label="Device Number">{license.deviceNumber}</Descriptions.Item>
-        <Descriptions.Item label="Disk Quota">{prettierBytes(license.diskQuota)}</Descriptions.Item>
+        <Descriptions.Item label="Expiry Date" className={hasExpired ? "expired" : ""}
+        >{expiryDate}</Descriptions.Item>
+        <Descriptions.Item label="Disk Quota" className={isOverQuota ? "overQuota" : ""}>
+          {isUnlimited ? "Unlimited" :
+            `${license.diskQuota} TB (Used: ${prettierBytes(bigintToNumber(usage))}, ${usagePercentage} of quota)`}
+        </Descriptions.Item>
         <Descriptions.Item label="Fingerprint">{license.fingerprint}</Descriptions.Item>
+
       </Descriptions>
     </>
   );
