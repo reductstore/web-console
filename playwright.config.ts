f9f26842096d9b2430b304d2d8a1e058ffa8ab92
Anthony <26444489+AnthonyCvn@users.noreply.github.com>
2026-04-07 22:34:20 +0200

    show progress in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 3 : 1,
-  reporter: process.env.CI ? "github" : "list",
+  reporter: process.env.CI ? [["github"], ["list"]] : "list",
   timeout: 60_000,
   use: {
     baseURL: process.env.E2E_BASE_URL ?? "http://localhost:5173/ui/",
