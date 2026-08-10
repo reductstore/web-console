200a50e694dd7e74bbe0b67297009c74b5b607a0
Alexey Timin <atimin@reduct.store>
2026-08-10 10:58:39 +0200

    Add replication destination prefix (#225)
    * add prefix to replication settings
    
    * Update changelog for PR #225
    
    * chore: update audited dependencies
    
    * ci: configure e2e authentication
    
    * Place replication prefix after bucket
    
    * Simplify replication prefix hint
    
    * Preserve masked replication tokens
    
    * ci: fix audit and e2e checks
    
    * revert token changes
    
    * set api token

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,10 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## Unreleased
 
+### Added
+
+- Support destination entry prefixes for replications, [PR-225](https://github.com/reductstore/web-console/pull/225)
+
 ## 1.15.1 - 2026-06-19
 
 ### Fixed
--- src/Components/Replication/ReplicationSettingsForm.tsx
+++ src/Components/Replication/ReplicationSettingsForm.tsx
@@ -47,6 +47,7 @@ interface FormValues {
   dstBucket: string;
   dstHost: string;
   dstToken: string;
+  dstPrefix?: string;
   entries: string[];
   recordSettings: Array<{
     action: FilterType;
@@ -83,8 +84,16 @@ export default class ReplicationSettingsForm extends React.Component<
    */
   onFinish = async (values: FormValues) => {
     const { replicationName, client, onCreated } = this.props;
-    const { srcBucket, dstBucket, dstHost, dstToken, entries, eachN, eachS } =
-      values;
+    const {
+      srcBucket,
+      dstBucket,
+      dstHost,
+      dstToken,
+      dstPrefix,
+      entries,
+      eachN,
+      eachS,
+    } = values;
     const include: Record<string, string> = {};
     const exclude: Record<string, string> = {};
     if (values.recordSettings) {
@@ -113,6 +122,7 @@ export default class ReplicationSettingsForm extends React.Component<
         dstBucket,
         dstHost,
         dstToken,
+        dstPrefix,
         entries,
         include,
         exclude,
@@ -245,6 +255,7 @@ export default class ReplicationSettingsForm extends React.Component<
       dstBucket: settings.dstBucket,
       dstHost: settings.dstHost,
       dstToken: settings.dstToken,
+      dstPrefix: settings.dstPrefix,
       entries: settings.entries || entries,
       recordSettings,
       when: formattedWhen,
@@ -383,6 +394,22 @@ export default class ReplicationSettingsForm extends React.Component<
               >
                 <Input disabled={readOnly} />
               </Form.Item>
+
+              <Form.Item
+                label={
+                  <span>
+                    Prefix&nbsp;
+                    <Tooltip title="Prepended to destination entry names.">
+                      <InfoCircleOutlined />
+                    </Tooltip>
+                  </span>
+                }
+                name="dstPrefix"
+                className="ReplicationField"
+              >
+                <Input disabled={readOnly} />
+              </Form.Item>
+
               <Form.Item
                 label={
                   <span>
