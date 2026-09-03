fe796d4671e4759489ce06077f5425124c728ebc
Marrii00 <211345279+Marrii00@users.noreply.github.com>
2026-09-03 10:53:35 +0000

    Tighten builder row label column

--- src/Components/QueryConditionBuilder/stepRowLayout.ts
+++ src/Components/QueryConditionBuilder/stepRowLayout.ts
@@ -1,4 +1,4 @@
-export const ROW_LABEL_WIDTH = 90;
+export const ROW_LABEL_WIDTH = 70;
 export const ROW_INPUT_WIDTH = 260;
 export const ROW_GAP = 8;
 export const ROW_GROUP_WIDTH = ROW_INPUT_WIDTH * 2 + ROW_GAP;
